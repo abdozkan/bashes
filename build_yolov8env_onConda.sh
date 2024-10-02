7a30884706967794d2b28a264e40e0fce7b2b1e8
Abdullah Özkan <58524436+abdozkan@users.noreply.github.com>
2024-10-02 17:50:28 +0300

    Update build_yolov8env_onConda.sh

--- build_yolov8env_onConda.sh
+++ build_yolov8env_onConda.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-conda create -n yolov8env_test python=3.10 -y
-source activate yolov8env_test
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+bash Miniconda3-latest-Linux-x86_64.sh
+
+conda create -n yolov8env python=3.10 -y
+source activate yolov8env
 
 python -m pip install --upgrade pip
 
@@ -9,4 +12,4 @@ pip install ultralytics==8.3.0 supervision inference
 conda install pytorch==2.4.0 torchvision==0.19.0 torchaudio==2.4.0 pytorch-cuda=12.4 -c pytorch -c nvidia -y
 pip install chardet
 
-echo "yolov8env created and installed dependencies"
+echo "miniconda & yolov8env created and installed dependencies"
